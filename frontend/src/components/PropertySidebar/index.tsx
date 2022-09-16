219bd81ccadd7fb52d944cfa51039a842bec9547
Kotauror <zygmunt.justyna@gmail.com>
2022-09-16 10:08:08 +0200

    Add closing of drawer.

--- frontend/src/components/PropertySidebar/index.tsx
+++ frontend/src/components/PropertySidebar/index.tsx
@@ -4,12 +4,57 @@ import { Drawer } from "antd";
 import { useState, useEffect } from "react";
 
 export const CloseIcon = () => {
+  return <div className="drawer-icon">{"◄"}</div>;
+};
+
+export const OpenIcon = ({ onClick }: { onClick: any }) => {
   return (
-    <div>
-      {"◄"}
-    </div>
-  )
-}
+    <button
+      className="back-button"
+      style={{
+        zIndex: 1002,
+        position: "absolute",
+        top: "calc(50% - 50px)",
+        height: "50px",
+        left: 0,
+        width: "20px",
+        fontSize: "x-small",
+        color: "#8d8d8d",
+        border: 0,
+        borderLeft: "1px solid rgb(218,220,224)",
+        boxShadow:
+          "0 1px 2px rgb(60 64 67 / 30%), 0 2px 6px 2px rgb(60 64 67 / 15%)",
+        borderRadius: "0 8px 8px 0",
+        cursor: "pointer"
+      }}
+      onClick={onClick}
+    >
+      {"►"}
+    </button>
+  );
+};
+
+export const RemoveIcon = ({ onClick }: { onClick: any }) => {
+  return (
+    <button
+      style={{
+        zIndex: 1002,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        margin: "0.5em",
+        color: "#535353",
+        border: "solid #8d8d8d",
+        borderRadius: "2px",
+        height: "2em",
+        width: "2em"
+      }}
+      onClick={onClick}
+    >
+      {"x"}
+    </button>
+  );
+};
 
 export const PropertySidebar = ({
   feature,
@@ -22,6 +67,10 @@ export const PropertySidebar = ({
 
   const onClose = () => {
     setVisible(false);
+  };
+
+  const onRemove = () => {
+    setVisible(false);
     setActiveFeature(undefined);
   };
 
@@ -29,30 +78,32 @@ export const PropertySidebar = ({
     setVisible(true);
   }, [feature]);
 
-  if (visible) {
-    return (
-      <div className="item-details">
-        <Drawer
-        //@ts-ignore ins
-          onClose={onClose}
-          getContainer={false}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 1001,
-            backgroundColor: "white",
-            height: "100%",
-            opacity: 0.9,
-          }}
-          open={visible}
-          closeIcon={<CloseIcon />}
-        >
-          {feature!.properties!.name}
-        </Drawer>
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
+  return (
+    <div className="item-details">
+      {visible && (
+        <div>
+          <RemoveIcon onClick={() => onRemove()} />
+          <Drawer
+            //@ts-ignore ins
+            onClose={onClose}
+            getContainer={false}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              zIndex: 1001,
+              backgroundColor: "white",
+              height: "100%",
+              opacity: 0.9
+            }}
+            open={visible}
+            closeIcon={<CloseIcon />}
+          >
+            {feature!.properties!.name}
+          </Drawer>
+        </div>
+      )}
+      {!visible && <OpenIcon onClick={() => setVisible(true)} />}
+    </div>
+  );
 };
