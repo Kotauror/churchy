a0e5a6d9869770ced53d266469f9062d4d85591c
Kotauror <zygmunt.justyna@gmail.com>
2022-11-28 12:32:17 +0100

    FE: showw more photos button

--- frontend/src/components/ImagesDisplayer/index.tsx
+++ frontend/src/components/ImagesDisplayer/index.tsx
@@ -6,6 +6,7 @@ export const ImagesDisplayer = (fetchedimagesData: {
   imagesMetaData: ImageMetaData[];
 }): JSX.Element => {
   const [urls, setUrls] = useState<string[]>([]);
+  const [showMorePhotos, setShowMorePhotos] = useState<boolean>(true);
   useEffect(() => {
     const workingUrls: string[] = [];
     fetchedimagesData.imagesMetaData.map(imageMetaData => {
@@ -18,14 +19,21 @@ export const ImagesDisplayer = (fetchedimagesData: {
 
     setUrls(workingUrls);
   }, [fetchedimagesData]);
-  
+
+  const buttonText = showMorePhotos ? "Zobacz więcej zdjęć" : "Ukryj zdjęcia";
+
   return (
     <div>
-      <ul>
-        {urls.map(url => {
-          return <img style={{width: "100%"}}src={url} key={url}></img>;
-        })}
-      </ul>
+      <button onClick={() => setShowMorePhotos(!showMorePhotos)}>
+        {buttonText}
+      </button>
+      {!showMorePhotos && (
+        <ul>
+          {urls.map(url => {
+            return <img style={{ width: "100%" }} src={url} key={url}></img>;
+          })}
+        </ul>
+      )}
     </div>
   );
 };
